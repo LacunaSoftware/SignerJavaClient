4af5cb118cab2c400a57c7e82f6960f949c48145
Jean Magalhães <jeanm@lacunasoftware.com>
2021-05-03 18:51:10 -0300

    Fixed setters from DocumentListParameters

--- src/main/java/com/lacunasoftware/signer/javaclient/params/DocumentListParameters.java
+++ src/main/java/com/lacunasoftware/signer/javaclient/params/DocumentListParameters.java
@@ -22,37 +22,42 @@ public class DocumentListParameters extends PaginatedSearchParams {
     public boolean getIsConcluded() {
         return this.IsConcluded;
     }
-    public boolean setIsConcluded(boolean IsConcluded) {
-        return this.IsConcluded = IsConcluded ;
+
+    public void setIsConcluded(boolean IsConcluded) {
+        this.IsConcluded = IsConcluded;
     }
 
 
     public UUID getFolderId() {
         return this.FolderId;
     }
-    public UUID setFolderId(UUID FolderId) {
-        return this.FolderId = FolderId ;
+
+    public void setFolderId(UUID FolderId) {
+        this.FolderId = FolderId;
     }
 
     public FolderType getFolderType() {
         return this.FolderType;
     }
-    public FolderType setFolderType(FolderType FolderType) {
-        return this.FolderType = FolderType ;
+
+    public void setFolderType(FolderType FolderType) {
+        this.FolderType = FolderType;
     }
 
-    public DocumentTypes DocumentTypes() {
+    public DocumentTypes getDocumentTypes() {
         return this.DocumentType;
     }
-    public DocumentTypes DocumentTypes(DocumentTypes DocumentTypes) {
-        return this.DocumentType = DocumentTypes ;
+
+    public void setDocumentTypes(DocumentTypes DocumentTypes) {
+        this.DocumentType = DocumentTypes;
     }
 
     public boolean getFilterByDocumentType() {
         return this.FilterByDocumentType;
     }
-    public boolean setFilterByDocumentType(boolean FilterByDocumentType) {
-        return this.FilterByDocumentType = FilterByDocumentType ;
+
+    public void setFilterByDocumentType(boolean FilterByDocumentType) {
+        this.FilterByDocumentType = FilterByDocumentType;
     }
 
 }
