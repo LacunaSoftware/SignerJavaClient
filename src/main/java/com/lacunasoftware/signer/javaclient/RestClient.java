cc9732cfff737a677264455963fdd0dd4facf4be
jrodriguesmagal <jrodriguesmagal@gmail.com>
2021-06-17 20:29:03 -0300

    Created UpdateInvoiceStatus method

--- src/main/java/com/lacunasoftware/signer/javaclient/RestClient.java
+++ src/main/java/com/lacunasoftware/signer/javaclient/RestClient.java
@@ -266,6 +266,39 @@ class RestClient {
 		return response;
 	}
 
+
+
+	<TRequest, TResponse> String putAsJson(String requestUri, TRequest request) throws RestException, IOException {
+
+		String verb = "PUT";
+		String requestUrl = resolveUrl(endpointUrl.toString(), requestUri);
+		HttpURLConnection conn;
+
+
+
+			URL url = new URL(requestUrl);
+			conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod(verb);
+			conn.setDoOutput(true);
+			conn.setRequestProperty("Content-Type", "application/json");
+			conn.setRequestProperty("Accept", "application/json");
+			conn.setRequestProperty("X-Api-Key", apiKey);
+			conn.setDoOutput(true);
+
+			OutputStream outStream = conn.getOutputStream();
+			if (request != null) {
+				String json = getGson().toJson(request);
+				outStream.write(json.getBytes());
+			}
+			outStream.close();
+			conn.getInputStream();
+			String message = conn.getResponseMessage();
+			return message;
+
+
+
+	}
+
 	void delete(String requestUri) throws RestException {
 
 		String verb = "DELETE";
--- src/main/java/com/lacunasoftware/signer/javaclient/SignerClient.java
+++ src/main/java/com/lacunasoftware/signer/javaclient/SignerClient.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.UUID;
 
 import com.lacunasoftware.signer.DocumentDownloadTypes;
+import com.lacunasoftware.signer.InvoicesUpdateInvoicePaymentStatusRequest;
 import com.lacunasoftware.signer.TicketModel;
 import com.lacunasoftware.signer.DocumentTicketType;
 import com.lacunasoftware.signer.documents.*;
@@ -226,6 +227,16 @@ public class SignerClient {
 		request.setDocumentId(documentId);
 		request.setFlowActionId(flowActionId);
 		getRestClient().post("/api/notifications/flow-action-reminder", request);
+	}
+
+
+	public String UpdateInvoiceStatus(int id, InvoicesUpdateInvoicePaymentStatusRequest request) throws RestException {
+		try {
+			return getRestClient().putAsJson(String.format("api/invoices/%s/payment", id), request);
+
+		}catch (Exception e){
+			return e.getMessage();
+		}
 	}
 
 	// endregion
