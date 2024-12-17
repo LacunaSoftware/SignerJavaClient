8857be427f8a1505050b2242b24181a8c1afe6b3
Danilo Bispo <danilobispogalvao@gmail.com>
2024-12-17 14:07:24 -0300

    Enhanced error handling in library for Signer API Errors. + Added *.gpg to .gitignore

--- src/main/java/com/lacunasoftware/signer/javaclient/exceptions/RestErrorException.java
+++ src/main/java/com/lacunasoftware/signer/javaclient/exceptions/RestErrorException.java
@@ -11,24 +11,38 @@ public class RestErrorException extends RestException {
 	private static final long serialVersionUID = 1L;
 	private int statusCode;
 	private String errorMessage;
+	private String errorCode;
 
 	public RestErrorException(String verb, String url, int statusCode) {
-		this(verb, url, statusCode, null);
+		this(verb, url, statusCode, null, null);
 	}
 
-	public RestErrorException(String verb, String url, int statusCode, String errorMessage) {
-		super(formatExceptionMessage(verb, url, statusCode, errorMessage), verb, url);
+	public RestErrorException(String verb, String url, int statusCode, String errorCode) {
+		this(verb, url, statusCode, null, errorCode);
+	}
+	
+	public RestErrorException(String verb, String url, int statusCode, String errorMessage, String errorCode) {
+		super(formatExceptionMessage(verb, url, statusCode, errorMessage, errorCode), verb, url);
 		this.statusCode = statusCode;
 		this.errorMessage = errorMessage;
+		this.errorCode = errorCode;
 	}
-
-	private static String formatExceptionMessage(String verb, String url, int statusCode, String errorMessage) {
+	
+	private static String formatExceptionMessage(String verb, String url, int statusCode, String errorMessage, String errorCode) {
+		StringBuilder message = new StringBuilder();
+		message.append(String.format("REST action %s %s returned HTTP error %d", verb, url, statusCode));
+		
 		if (errorMessage != null) {
-			return String.format("REST action %s %s returned HTTP error %d: %s", verb, url, statusCode, errorMessage);
-		} else {
-			return String.format("REST action %s %s returned HTTP error %d", verb, url, statusCode);
+			message.append(String.format(": %s", errorMessage));
 		}
+		
+		if (errorCode != null) {
+			message.append(String.format(" (Error Code: %s)", errorCode));
+		}
+		
+		return message.toString();
 	}
+	
 
 	public int getStatusCode() {
 		return statusCode;
@@ -36,5 +50,9 @@ public class RestErrorException extends RestException {
 
 	public String getErrorMessage() {
 		return errorMessage;
+	}
+
+	public String getErrorCode() {
+		return errorCode;
 	}
 }
